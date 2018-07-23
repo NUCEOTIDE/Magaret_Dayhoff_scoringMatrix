67e01c304eac922871c8d06cc4daeb455c9aa918
Jeff Dui <1174980556@qq.com>
2018-07-23 17:18:01 +0800

    Update: constructor building
    built a constructor special for homework

--- src/Magaret_Dayhoff_socringMatrix.java
+++ src/Magaret_Dayhoff_socringMatrix.java
@@ -1,6 +1,10 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Magaret_Dayhoff_socringMatrix {
     String[] scoringSource;
     int sourceDepth;
+    char[] syllabus;
 
 
     public Magaret_Dayhoff_socringMatrix(String type){
@@ -9,7 +13,27 @@ public class Magaret_Dayhoff_socringMatrix {
             default:
         }
     }
+
     public Magaret_Dayhoff_socringMatrix(int sourceDepth){
         this.sourceDepth=sourceDepth;
+        scoringSource=new String[sourceDepth];
+        Scanner s=new Scanner(System.in);
+        try{
+            System.out.println("Please input syllabus: (separate them with space, end inputting with enter)");
+            String rawSyllabus=s.nextLine();
+            rawSyllabus.replaceAll(" ","");
+            syllabus=new char[rawSyllabus.length()];
+            for(int i=0;i<rawSyllabus.length();i++){
+                syllabus[i]=rawSyllabus.charAt(i);
+            }
+            System.out.println("Please input source: (separate them with enter)");
+            for(int i=0;i<scoringSource.length;i++) scoringSource[i]=s.nextLine();
+        }catch (Exception e){
+
+        }finally {
+            System.out.println("All inputs are accepted, object constructed");
+        }
     }
+
+    private void 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        Magaret_Dayhoff_socringMatrix homewoark=new Magaret_Dayhoff_socringMatrix();
+        Magaret_Dayhoff_socringMatrix homewoark=new Magaret_Dayhoff_socringMatrix(6);
     }
 }
